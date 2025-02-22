b3a6e658a6dd4b6fd6157934590566a1ccd7d54c
Mubeen Nasir <mubeennasir117@gmail.con>
2025-02-23 00:17:59 +0500

    l

--- src/autoaudio.js
+++ src/autoaudio.js
@@ -3,7 +3,6 @@ const fs = require('fs-extra');
 const path = require('path');
 const { Boom } = require('@hapi/boom');
 
-<<<<<<< HEAD
 const escapeRegExp = (string) => {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 =======
